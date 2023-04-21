45c51fbb4719ee4e907fef2fbbfafa4cb5e159d6
kelvins213 <kelvinotilio@gmail.com>
2023-04-21 02:22:15 -0300

    added carousel_slide view on films_available.dart

--- lib/pages/fims_avaliable.dart
+++ lib/pages/fims_avaliable.dart
@@ -2,7 +2,6 @@ import 'package:cinema/data/api/get/films.dart';
 import 'package:cinema/data/shared_preferences/booking_shared_preferences.dart';
 import 'package:cinema/data/shared_preferences/costumer_shared_preferences.dart';
 import 'package:cinema/domain/film.dart';
-import 'package:cinema/widget/reservs.dart';
 import 'package:cinema/pages/login_page.dart';
 import 'package:cinema/widget/booking.dart';
 import 'package:flutter/material.dart';
